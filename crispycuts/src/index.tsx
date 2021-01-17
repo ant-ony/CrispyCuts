be17c13311b14f43fb98e9e68613676ed862d58f
Antony Tokarr <antonytokarr@gmail.com>
2021-01-17 00:23:57 -0500

    cleaned up all files

--- crispycuts/src/index.tsx
+++ crispycuts/src/index.tsx
@@ -4,13 +4,6 @@ import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
@@ -27,7 +20,6 @@ const Body= ()=>{
         description='Lorem ipsum dolor sit amet...'
       
       />
-      <ContactContainer/> 
     </div>
   )
 }
@@ -66,3 +58,10 @@ const Card = (props) => {
     </div>
   )
 }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Body />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
