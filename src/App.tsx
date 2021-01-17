9fa3a36e878452f47410e07a1ce36323855c67ed
Antony Tokarr <antonytokarr@gmail.com>
2021-01-17 18:11:29 -0500

    Added Burger and Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Home } from "./pages/Home";
 import { NavLayout } from "./components/Nav";
+import { Burger, Menu } from "./components";
 import Media from "./Media";
 
 const App = () => (
